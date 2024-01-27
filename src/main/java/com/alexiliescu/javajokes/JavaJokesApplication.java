ed2630b11fc7bc150582fa11032c880217577530
Alex <alexandru.iliescu@fortechproducts.ro>
2024-01-27 17:52:00 +0200

    Project setup

---
+++ src/main/java/com/alexiliescu/javajokes/JavaJokesApplication.java
@@ -0,0 +1,12 @@
+package com.alexiliescu.javajokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJokesApplication.class, args);
+    }
+}
---
+++ src/test/java/com/alexiliescu/javajokes/JavaJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiliescu.javajokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
